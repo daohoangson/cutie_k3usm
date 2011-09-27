891817c17aae10080f05a80e6f56ba7f04f32f24
Dao Hoang Son <daohoangson@gmail.com>
2011-09-28 02:51:43 +0700

    post.php now works with fb_uid from query string

--- post.php
+++ post.php
@@ -3,6 +3,14 @@
 require_once('includes/init.php');
 
 $user = Model_User::findUserByUsername('sondh');
+if (empty($user)) {
+	die('Please login');
+}
+
+if (empty($_REQUEST['fb_uid'])) {
+	die('Please give me some fb_uid');
+}
+$fb_uid = $_REQUEST['fb_uid'];
 
 $app = App::getInstance();
 $session = $app->get('session');
@@ -12,7 +20,7 @@ $facebook = Api_Facebook::getInstance();
 $session->setupFacebook($facebook);
 
 $result = $facebook->api('/me/kthreeusm:kiss', 'post', array(
-	'profile' => $app->getFileUrl('profile.php?fb_uid=lotterite'),
+	'profile' => $app->getFileUrl('profile.php?fb_uid=' . $fb_uid),
 ));
 
 var_dump($result);
